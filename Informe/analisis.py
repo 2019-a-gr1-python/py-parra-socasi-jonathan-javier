9017a044c32da60def51d85f51518bd125742521
Jonathan <jonathan01.07.95@gmail.com>
2019-06-02 18:31:16 -0500

    pandas

--- Informe/analisis.py
+++ Informe/analisis.py
@@ -43,66 +43,8 @@ mejores_arqueros = lista_arqueros.sort_values(ascending = [False, False], by = [
 
 print(mejores_arqueros)
 
-# Los mejores jugadores ecuatorianos
+# Los 10 mejores jugadores ecuatorianos en el juego de FIFA19
 
 lista_ecuatorianos = pd.DataFrame(lista_jugadores, columns=['Name', 'Overall', 'Club', 'Nationality'])
 ecuatorianos_mejor_puntuados = lista_ecuatorianos.sort_values(ascending = [False], by = ['Overall']).query('Nationality == "Ecuador"')
 print(ecuatorianos_mejor_puntuados.head(10))
-
-
-# personajes_smashito_melee = pd.DataFrame(personajes['Melee']).count()
-
-
-"""
-
-pokemonDescendenteTotal = pokemons.sort_values(ascending = False, by = 'Total')
-
-print('Pokemons màs poderosos')
-print(pokemonDescendenteTotal.loc[:,['Name','Total','Type 1','Generation','Attack', 'Defense']].head(5))
-print('Pokemons más debiles')
-print(pokemonDescendenteTotal.loc[:,['Name','Total','Type 1','Generation','Attack', 'Defense']].tail(5))
-
-pokemonDescendenteDefense = pokemons.sort_values(ascending = False, by = 'Defense')
-
-
-plot.figure(2)
-plot.bar(universos, numero_personajes_por_universo)
-plot.xticks(rotation=90)
-plot.xlabel('Universos')
-plot.ylabel('No. de personajes por universo')
-plot.title('Personajes por universo')
-
-
-
-tiposPokemon = pokemons['Type 1'].unique()
-cantidadPokemonsXTipo = np.zeros(tiposPokemon.size);
-for idx, tipo in enumerate(tiposPokemon):
-    cantidadPokemonsXTipo[idx] = pokemons[pokemons['Type 1'] == tipo].Name.count()
-
-
-
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-columnas_a_usar = ['name','ID','ALIGN','SEX','GSM','ALIVE','APPEARANCES','FIRST APPEARANCE','YEAR']
-contenido_archivo = pd.read_csv(
-    'dc-data.csv',
-    usecols=columnas_a_usar
-    )
-
-df_gropu_good = contenido_archivo.groupby("ALIGN")
-print(df_gropu_good)
-print(type(df_gropu_good))
-valores = []
-valore = []
-for name , df_agrupado in df_gropu_good:    
-    valores.append(name)
-    valore.append(len(df_agrupado.index))
-    
-
-plt.xlabel('En x')
-plt.xlabel('En y')
-plt.plot(valores,valore)
-"""
